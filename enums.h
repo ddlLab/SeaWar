5d56f0998b20946eaaf2b106d0bc52dcbca09176
akaten-ok <akaten.ru@gmail.com>
2020-09-22 20:35:56 +0300

    '

--- enums.h
+++ enums.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <string>
+using namespace std;
+
+enum class eCellType
+{
+	EMPTY,
+	ALIVE,
+	HITTED,
+	MISSED
+}; 
+
+enum class eTokenType
+{
+	COMMAND,
+	POSSITION,
+	UNKNOWN
+};
